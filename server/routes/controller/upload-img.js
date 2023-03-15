b1238314dc24f4fffc61568210f4799f627e003b
jsnnarvz <narvaezjason024@gmail.com>
2023-03-16 05:22:12 +0800

    Update: Upload User Img

--- server/routes/controller/upload-img.js
+++ server/routes/controller/upload-img.js
@@ -1,76 +1,56 @@
-const { Router } = require('express');
-const { initializeApp } = require ('firebase/app');
-const multer = require ('multer');
-const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require ('firebase/storage');
-const firebaseConfig = {
-    apiKey: "AIzaSyCHFx0B0krQVivgCkaOPhLmMx8tfdV7Qso",
-    authDomain: "recifil.firebaseapp.com",
-    databaseURL: "https://recifil-default-rtdb.firebaseio.com",
-    projectId: "recifil",
-    storageBucket: "recifil.appspot.com",
-    messagingSenderId: "986428398442",
-    appId: "1:986428398442:web:d43a57605bffe2f3a6a313",
-    measurementId: "G-BF98SLQW1L"
-  };
+const { Router, res, req } = require('express');
+const multer = require('multer');
+const fs = require ('fs');
+
+const { getst } = require ('../../firebase/index')
+
+const ProfileImg = Router();
+
+
+const upload = multer();
+const bucket = getst.bucket();
+
+ProfileImg.post('/upload',upload.single('Profile'), (req, res, next) => {
+
+const folderName = 'file';
+const folderName2 = 'User_Profile';
+const fileName = req.file.originalname ;
+const fileObjectPath = `${folderName}/${folderName2}/${fileName}`;
+  const file = req.file;
+  if (!file) {
+    return next(new Error('No file uploaded.'));
+  }
+
+  // Upload the file to Firebase Storage
+  const fileUpload = bucket.file(fileObjectPath);
+  const stream = fileUpload.createWriteStream({
+    metadata: {
+      contentType: file.mimetype
+    }
+  });
+
+  stream.on('error', (err) => {
+    req.file.cloudStorageError = err;
+    next(err);
+  });
+
+  stream.on('finish', () => {
+    req.file.cloudStorageObject = fileObjectPath;
+    fileUpload.makePublic()
+      .then(() => {
+        req.file.cloudStoragePublicUrl = `https://storage.googleapis.com/${bucket.name}/${fileObjectPath}`;
+        res.send('Image are Uploaded!!')
+      })
+      .catch((err) => {
+        req.file.cloudStorageError = err;
+        next(err);
+      });
+  });
+
+  stream.end(file.buffer);
+
+  });
 
 
 
-const uploadImg = Router();
-//initialize a firebase application
-const firebaseApp = initializeApp(firebaseConfig);
-
-//Initialize Cloud Storage and get a reference to the service 
-const storage = getStorage(firebaseApp);
-
-
-const upload = multer({ storage: multer.memoryStorage(), dest: 'file/' });
-uploadImg.post('/',upload.single("filename"), async (req,res)=>{
-    console.log("Upload is Running")
-    try {
-        
-        const dateTime = giveCurrentDateTime();
-
-        const storageRef = ref(storage, req.file.originalname + "   " + dateTime)
-
-        //Create file metadata including the content type
-        const metadata = {
-            contentType: req.file.mimetype,
-        }
-
-        //Upload the file in the bucket bucket storage 
-        const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
-
-        //Grab the Public Url 
-        const downloadURL = await getDownloadURL(snapshot.ref); 
-
-        console.log('File successfully uploaded.');
-        return res.send({
-            message: 'file uploaded to firebase storage',
-            name: req.file.originalname,
-            type: req.file.mimetype,
-            downloadURL: downloadURL
-        })
-
-
-    } catch (error) {
-        return res.status(400).send(error.message);
-    } 
-});
-
-const giveCurrentDateTime = () => {
-    const today = new Date();
-    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDay() + 1);
-    const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds() ;
-    const dateTime = date + ' ' + time ;
-    return dateTime;
-} 
-
-
-uploadImg.get('/upload', (req, res) => {
-  res.json("Storage");
-})
-
-///
-
-
-exports.ImgController = uploadImg;
+exports.UploadImg = ProfileImg;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express= require ('express');
 const {res, req} = require ('express');
-const rootrouter = require ('./routes/controller/CRUD-UserAccount');
 const UserPost = require ('./routes/controller/User-post');
-const uploadImg = require ('./routes/controller/upload-img')
+const ProfileImg = require ('./routes/controller/upload-img')
 const SignUp = require ('./routes/controller/Signup-Auth')
 const tryUploadImg = require ('./routes/controller/UserPost_Img')
 
@@ -17,10 +16,9 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log('"/home" root is for landing page');
-    app.use('/user',rootrouter.routersignup)
-    app.use('/postrecipe',UserPost.PostRecipe)
-    app.use('/user/UploadProfile',uploadImg.ImgController)
-    app.use('/user/signup',SignUp.UserAccount)
-    app.use('/tryupload', tryUploadImg.ImgTrial)
-    console.log('Server running in the Port: ' + PORT)
+    app.use('/postrecipe',UserPost.PostRecipe);
+    app.use('/user/UploadProfile',ProfileImg.UploadImg);
+    app.use('/user/signup',SignUp.UserAccount);
+    app.use('/tryupload', tryUploadImg.ImgTrial);
+    console.log('Server running in the Port: ' + PORT);
 })
