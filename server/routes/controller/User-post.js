e1f844900e8dd37ecb55f52cba301ff7d68de0b2
jsnnrv <narvaezjason024@gmail.com>
2023-03-18 01:18:09 +0800

    Update UserPost Recipe

--- server/routes/controller/User-post.js
+++ server/routes/controller/User-post.js
@@ -1,37 +1,35 @@
 
-const { req, res, Router } = require('express');
-const User_PostRecipe = require ('../../schema/UserPostRecipe')
+const { req, res, Router, response } = require('express');
+const User_PostRecipe = require ('../../schema/UserPostRecipe');
+const multer = require('multer');
 
 // //For database
 const { db } = require ('../../firebase/index');
 const { body } = require('express-validator');
 
 
-
+const upload = multer();
 const UserPost = Router();
 
 
-UserPost.post('/',
+UserPost.post('/',upload.none(),
 async (req, res) => {
         
   try{
     console.log("Userpost Success");
+;
     const UserJson= {
           RecipeName: req.body.RecipeName,
           RecipeAbout: req.body.RecipeAbout,
           Main_Ingredients:req.body.Main_Ingredients,
           Category:req.body.Category,
-          Ingredients:req.body.Ingredients,
-          Measurements:req.body.Measurements,
-          Status: ("Status") != true
+          Ingredients_map: {
+          Ingredients : req.body.Ingredients, 
+          Measurements: req.body.Measurements
+          },
+          Status: ("Status") == false
     }
-    const rp = req.body.RecipeProcess;
-    console.log(UserJson);
-    console.log(Ingredients, Measurements);
-    const response = await db.collection("UserPost").add(UserJson).doc('Ingredients_map').set({
-      Ingredients: req.body.Ingredients,
-      Measurements: req.body.Measurements
-    });
+    const response = await db.collection("UserPost").add(UserJson);
     res.send(UserJson);
   }
   catch(error){
@@ -40,7 +38,26 @@ async (req, res) => {
 })
 UserPost.get('/getpost',
 async (req, res) => {
-        
+        // try {
+        //   const docRef = await db.collection('UserPost').doc('t6PSDo8FcCg3eLBtHDIi');
+
+        //     docRef.get().then((doc) => {
+        //       if (doc.exists) {
+        //         const Ingredients_map = doc.data().Ingredients_map;
+        //         const Ingredients = doc.data().Ingredients_map.Ingredients;
+        //         const Measurements = doc.data().Ingredients_map.Measurements;
+        //              console.log(`Ingredients: ${[Ingredients]}`);
+        //              console.log(`Mesurements: ${[Measurements]}`);
+        //             res.send(Ingredients_map);
+        //       } else {
+        //        console.log('No such document!');
+        //        }
+        //   })
+        // }
+        //   catch(error){
+        //      console.log('Error getting document:', error);
+        //       }
+            
   try {
     const UserPost = db.collection("UserPost");
     const response = await UserPost.get();
@@ -55,8 +72,6 @@ async (req, res) => {
   catch(error){
     res.send(error)
   }
-
-
 })
 
 
