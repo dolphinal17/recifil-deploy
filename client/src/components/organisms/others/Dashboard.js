6b068a9e91fe2eceb0729022ef043ea7917f25b4
Ivan Delfin <delfin.ivan.bscs2019@gmail.com>
2023-05-03 12:49:39 +0800

    dashboard length

--- client/src/components/organisms/others/Dashboard.js
+++ client/src/components/organisms/others/Dashboard.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { db } from '../../../config/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
+
 
 export default function Dashboard() {
+
+
+    const [userCount, setUserCount] = useState(0);
+    const [postCount, setPostCount] = useState(0);
+    const [recipesCount, setRecipesCount] = useState(0);
+    const [ingredientsCount, setIngredientsCount] = useState(0);
+
+    useEffect(() => {
+      const fetchUserCount = async () => {
+        const querySnapshot = await getDocs(collection(db, "userinfo"));
+        setUserCount(querySnapshot.size);
+      };
+      fetchUserCount();
+    }, []);
+
+    useEffect(() => {
+        const fetchPostCount = async () => {
+          const querySnapshot = await getDocs(collection(db, "createpost"));
+          setPostCount(querySnapshot.size);
+        };
+        fetchPostCount();
+      }, []);
+
+      useEffect(() => {
+        const fetchRecipesCount = async () => {
+          const querySnapshot = await getDocs(collection(db, "recipes"));
+          setRecipesCount(querySnapshot.size);
+        };
+        fetchRecipesCount();
+      }, []);
+
+      useEffect(() => {
+        const fetchIngCount = async () => {
+          const querySnapshot = await getDocs(collection(db, "ingdata"));
+          setIngredientsCount(querySnapshot.size);
+        };
+        fetchIngCount();
+      }, []);
+
+
   return (
     <div className='w-full flex flex-row items-center justify-center flex-wrap gap-[2rem] m-auto'>
         <Link to="/adminuser">
@@ -10,7 +54,7 @@ export default function Dashboard() {
                 <img src='https://firebasestorage.googleapis.com/v0/b/recifil.appspot.com/o/webimages%2Fman.png?alt=media&token=7132aeeb-7c45-4b23-9a09-b86b719cd774' className='w-[3rem]' />
             </div>
             <div className='flex flex-col justify-center items-center'>
-                <span className='text-[2.875rem] font-bold text-[#464255]'>75</span>
+                <span className='text-[2.875rem] font-bold text-[#464255]'>{userCount}</span>
                 
                 <p className='text-[1rem] font-normal text-[#464255]'>Total Users</p>
             </div>
@@ -23,7 +67,7 @@ export default function Dashboard() {
             <img src='https://firebasestorage.googleapis.com/v0/b/recifil.appspot.com/o/webimages%2Flike.png?alt=media&token=c34080da-0d64-4cff-a126-4c0521edc02a' className='w-[3rem] h-[2.688rem]' />
             </div>
             <div className='flex flex-col justify-center items-center'>
-                <span className='text-[2.875rem] font-bold text-[#464255]'>75</span>
+                <span className='text-[2.875rem] font-bold text-[#464255]'>{postCount}</span>
                 <p className='text-[1rem] font-normal text-[#464255]'>Total Posts</p>
             </div>
         </div>
@@ -35,7 +79,7 @@ export default function Dashboard() {
             <img src='https://firebasestorage.googleapis.com/v0/b/recifil.appspot.com/o/webimages%2Frecipe-book.png?alt=media&token=753ead30-2959-4455-9c86-487eb9be84f1' className='w-[3rem] h-[2.688rem]' />
             </div>
             <div className='flex flex-col justify-center items-center'>
-                <span className='text-[2.875rem] font-bold text-[#464255]'>75</span>
+                <span className='text-[2.875rem] font-bold text-[#464255]'>{recipesCount}</span>
                 <p className='text-[1rem] font-normal text-[#464255]'>Total Recipes</p>
             </div>
         </div>
@@ -46,7 +90,7 @@ export default function Dashboard() {
             <img src='https://firebasestorage.googleapis.com/v0/b/recifil.appspot.com/o/webimages%2Fgrocery.png?alt=media&token=fd661c24-bfa1-4161-a4f9-f1f32418950e' className='w-[3rem] h-[2.688rem]' />
             </div>
             <div className='flex flex-col justify-center items-center'>
-            <span className='text-[2.875rem] font-bold text-[#464255]'>75</span>
+            <span className='text-[2.875rem] font-bold text-[#464255]'>{ingredientsCount}</span>
             <p className='text-[1rem] font-normal text-[#464255]'>Total Ingredients</p>
             </div>
         </div>
